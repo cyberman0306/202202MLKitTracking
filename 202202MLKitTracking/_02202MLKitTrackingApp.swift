8a23730cbf5c84e8460906b18c54f1b2823abab4
HYUN JUN YOOK <yhj03069@gmail.com>
2023-02-10 11:06:12 +0900

    Initial Commit

---
+++ 202202MLKitTracking/_02202MLKitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _02202MLKitTrackingApp.swift
+//  202202MLKitTracking
+//
+//  Created by cnu on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct _02202MLKitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
